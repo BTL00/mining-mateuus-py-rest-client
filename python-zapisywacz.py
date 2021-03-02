063e11bc17d7c3bd8700e0182de737b7f4c891df
Piotr Bomba <bombapiotr@gmail.com>
2021-03-02 01:01:21 +0100

    Add files via upload

--- python-zapisywacz.py
+++ python-zapisywacz.py
@@ -7,9 +7,8 @@ import re
 patternTrex = ".*\[\sOK\s\].*"
 patternGminer = ".*accepted.*"
 
-def doPOST(id):
-	url = 'https://mining.mateu.us/share?id='+str(id)
-	x = requests.post(url, data = {})
+def doPOST(id, url, password):
+	x = requests.post(url, data = {'id': id, 'pass': password})
 	if(x.status_code == 200):
 		print("Send POST -> Ok!")
 	else:
@@ -17,14 +16,14 @@ def doPOST(id):
 
 
 if __name__ == "__main__":
-	if (len(sys.argv) < 3 or sys.argv[1] == "--help"):
-		print("args: command user_id")
+	if (len(sys.argv) < 5 or sys.argv[1] == "--help"):
+		print("args: command user_id ur pass")
 		exit()
 	datestring = datetime.today().strftime('%Y-%m-%d-%H%M%S')
 	with open(datestring+'.log', 'wb') as f:  # replace 'w' with 'wb' for Python 3
 		process = subprocess.Popen(sys.argv[1], stdout=subprocess.PIPE)
 		for line in process.stdout:  # replace '' with b'' for Python 3
 			if (re.match(patternTrex, str(line)) or re.match(patternGminer, str(line))):
-				doPOST(sys.argv[2])
+				doPOST(sys.argv[2], sys.argv[3], sys.argv[4])
 			print(line)
 			f.write(line)
